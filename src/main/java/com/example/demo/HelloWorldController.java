6ec91b8db432722065645d8ffbb417606bb2304f
Sudheer Bijjaragi <sudheer.bijjaragi@gmail.com>
2023-06-28 16:17:33 +0200

    first commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "This is basic sample springboot setup for reuse";
+    }
+
+    @RequestMapping("/first")
+    public String first()
+    {
+        return "Hello first endpoint";
+    }
+}
